89045c8f9314d1980419f51b25737753a3e9ec91
Onur Güvensoy <gvnsynr@gmail.com>
2024-10-31 00:38:47 +0300

    auth

--- server/index.js
+++ server/index.js
@@ -3,12 +3,13 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const app = express();
-require("dotenv").config();
+const cookieParser = require("cookie-parser");
+const authRoute = require("./Routes/AuthRoute");
 const MONGO_URL ="mongodb+srv://admin:Fb1907Fb@forum-app.mcv3k.mongodb.net/?retryWrites=true&w=majority&appName=forum-app";
 const PORT = 4006;
 mongoose.connect(MONGO_URL, {
   
-  
+
   }).then(() => console.log("MongoDB is  connected successfully"))
   .catch((err) => console.error(err));
 
@@ -23,4 +24,8 @@ app.use(
     credentials: true,
   }));
 
-app.use(express.json());
+  app.use(cookieParser());
+
+  app.use(express.json());
+  
+  app.use("/", authRoute);
