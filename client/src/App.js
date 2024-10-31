82a6a37c3ebc3cfb1b6e5dcc29fdbe90faa11a1d
Onur Güvensoy <gvnsynr@gmail.com>
2024-10-31 19:06:30 +0300

    client-side structured.

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,16 @@
+import { Route, Routes } from "react-router-dom";
+import { Login, Signup } from "./pages";
+import Home from "./pages/Home";
+
 function App() {
   return (
-   <h1>onur guvensoy</h1>
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+    </div>
   );
 }
 
