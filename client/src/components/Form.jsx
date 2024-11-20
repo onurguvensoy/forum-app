76119aa55db7a543d5ed5229bbe5774f9483ab4c
Onur Güvensoy <gvnsynr@gmail.com>
2024-11-21 00:21:17 +0300

    some fixes.

--- client/src/components/Form.jsx
+++ client/src/components/Form.jsx
@@ -1,16 +1,25 @@
-import {React,useState}from 'react'
+import React, {useState,useEffect} from 'react';
 import axios from "axios";
 import { ToastContainer, toast } from "react-toastify";
-
 const Form = () => {
   const [inputValue, setInputValue] = useState({
     title: "",
     content: "",
   });
-  const [username,setUsername] = useState("");
-  
-
   const { title, content } = inputValue;
+  const [username, setUsername] = useState("")
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const { data } = await axios.get("http://localhost:4000/user");
+        setUsername(data.username);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchData();
+  }, []);
   const handleOnChange = (e) => {
     const { name, value } = e.target;
     setInputValue({
@@ -18,40 +27,42 @@ const Form = () => {
       [name]: value,
     });
   };
-  const handleError =  (err) => 
-    toast.error(err,{
+
+  const handleError = (err) =>
+    toast.error(err, {
       position: "top-right",
-      autoClose:2000,
+      autoClose: 2000,
       closeButton: false,
       hideProgressBar: false,
       theme: "dark",
     });
-    const handleSuccess = (msg) =>
-      toast.success(msg, {
-        position: "top-right",
-        autoClose: 2000,
-        closeButton: false,
-        hideProgressBar: false,
-        theme: "dark",
-      });
-    
-  const handleSubmit = async(e) => {
+
+  const handleSuccess = (msg) =>
+    toast.success(msg, {
+      position: "top-right",
+      autoClose: 2000,
+      closeButton: false,
+      hideProgressBar: false,
+      theme: "dark",
+    });
+
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
     try {
       const { data } = await axios.post(
         "http://localhost:4000/entry",
         {
           ...inputValue,
-         username,
-  
+          username,
         },
         { withCredentials: true }
       );
+  
       const { success, message } = data;
       if (success) {
         handleSuccess(message);
-        setTimeout(() => {
-        }, 1000);
+        setTimeout(() => {}, 1000);
       } else {
         handleError(message);
       }
@@ -59,26 +70,35 @@ const Form = () => {
       console.log(error);
     }
     setInputValue({
-      ...inputValue,
       title: "",
-      content:""
+      content: "",
     });
   };
 
-
   return (
-
     <div>
       <form onSubmit={handleSubmit}>
         <label htmlFor="title">Title:</label>
-        <input type="text" id="title" name="title" value={title} placeholder='Please Type...' onChange={handleOnChange}/>
+        <input
+          type="text"
+          id="title"
+          name="title"
+          value={title}
+          placeholder="Please Type..."
+          onChange={handleOnChange}
+        />
         <label htmlFor="content">Content:</label>
-        <input id="content" name="content" value={content} onChange={handleOnChange}></input>
-        <button type="submit" onClick={handleSubmit}>Submit</button>
+        <input
+          id="content"
+          name="content"
+          value={content}
+          onChange={handleOnChange}
+        />
+        <button type="submit">Submit</button>
       </form>
       <ToastContainer />
     </div>
-  )
-}
+  );
+};
 
-export default Form
+export default Form;
